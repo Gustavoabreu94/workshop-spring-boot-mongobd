f4843dc7673969736384c0cd9a5c7f1db3bd3b33
Gustavo Alves Abreu <113162142+Gustavoabreu94@users.noreply.github.com>
2022-10-10 12:19:55 -0300

    Frist commit - Project Create

---
+++ workshopmongo/src/main/java/com/gustavoabreu/workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.gustavoabreu.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
